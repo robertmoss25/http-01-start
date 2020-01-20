06c60c8aee45993974ee10eaddef11054285110b
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2020-01-20 15:53:29 -0500

    Using Rxjs to transform data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -37,7 +38,17 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get('https://ng-complete-guide-48ed3.firebaseio.com/posts.json').subscribe(posts => {
+    this.http.get('https://ng-complete-guide-48ed3.firebaseio.com/posts.json')
+    .pipe(map(responseData => {
+      const postsArray = [];
+      for(const key in responseData) {
+        if (responseData.hasOwnProperty(key)) {
+          postsArray.push({ ...responseData[key], id: key});
+        }
+      }
+      return postsArray;
+    }))
+    .subscribe(posts => {
       console.log(posts);
     });
   }
